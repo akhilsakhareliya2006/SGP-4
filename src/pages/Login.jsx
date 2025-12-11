9dbc0b6a359505744f76a4ddecf159a29469554a
HetSavani36 <hetsavani36@gmail.com>
2025-12-11 21:04:32 +0530

    integrated login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import TextInput from "../components/TextInput.jsx";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate=useNavigate()
   const [form, setForm] = useState({
     email: "",
     password: "",
@@ -30,14 +32,49 @@ const Login = () => {
     return Object.keys(newErrors).length === 0; // true if no errors
   };
 
-  const handleSubmit = (e) => {
+const handleSubmit = async (e) => {
     e.preventDefault();
     if (!validate()) return;
 
-    // TODO: call backend API here later
-    console.log("Login data:", form);
-    alert("Login form submitted (frontend only now)");
+    try {
+      
+      const res = await fetch("http://localhost:5000/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form)
+      });
+
+      const data = await res.json();  // <-- IMPORTANT
+
+      if (!res.ok) {
+        console.error("Error:", data);
+        alert(data.message || "login failed");
+        return;
+      }
+
+      console.log("Logged In Successfully:", data);
+      alert("Logged In successfully!");
+
+      setForm({
+        email: "",
+        password: "",
+      });
+
+    // 2️⃣ Redirect to login page
+      if(data.data.user.role==="collegeAdmin") navigate("/college/admin", { replace: true });
+      if(data.data.user.role==="student") navigate("/college/student", { replace: true });
+      if(data.data.user.role==="mentor") navigate("/college/mentor", { replace: true });
+      if(data.data.user.role==="companyAdmin") navigate("/company/admin", { replace: true });
+      if(data.data.user.role==="employee") navigate("/company/employee", { replace: true });
+
+    } catch (error) {
+      console.error("Network error:", error);
+      alert("Something went wrong!");
+    }
   };
+
 
   return (
     <div className="page-container">
